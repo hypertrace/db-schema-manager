b7e6ce7a57e2fd2132a7e97de0c1da5bdd9e1b70
Ravi Singal <ravi.singal@traceable.ai>
2022-07-22 01:02:11 +0530

    update gradle plugin repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,8 @@
 pluginManagement {
   repositories {
     mavenLocal()
-    maven((extra.properties["artifactory_contextUrl"] as String) + "/gradle") {
-      credentials {
-        username = extra.properties["artifactory_user"] as String
-        password = extra.properties["artifactory_password"] as String
-      }
-    }
+    gradlePluginPortal()
+    maven("https://hypertrace.jfrog.io/artifactory/maven")
   }
 }
 
